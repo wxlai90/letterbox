b2b22898d8ff2f11df1c81e907de99443447550f
lwx <l@w.x>
2020-08-11 20:52:11 +0800

    Added my own responsive navbar. Changed color to white.
    Changed width of media queries to accomodate QR code.

--- client/src/components/Header/header.component.jsx
+++ client/src/components/Header/header.component.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import './header.styles.css'
+
 import { Link } from 'react-router-dom'
 
 
@@ -11,18 +13,33 @@ const Header = () => {
 
 
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark" style={styles}>
-            <Link to="/" className="navbar-brand">Letterbox</Link>
-            <ul className="navbar-nav ml-auto">
-                <li className="nav-item active">
+        <nav>
+            <div>
+                <Link to="/" className="navbar-brand">Letterbox</Link>
+            </div>
+
+            <ul>
+                <li>
                     <Link to="/" className="nav-link">Home</Link>
                 </li>
-                <li className="nav-item active">
+                <li>
                     <Link to="/box/new" className="nav-link">Get a box</Link>
                 </li>
             </ul>
         </nav>
     )
 }
+
+// <nav className="navbar navbar-expand-lg navbar-dark" style={styles}>
+// <Link to="/" className="navbar-brand">Letterbox</Link>
+// <ul className="navbar-nav ml-auto">
+//     <li className="nav-item active">
+//         <Link to="/" className="nav-link">Home</Link>
+//     </li>
+//     <li className="nav-item active">
+//         <Link to="/box/new" className="nav-link">Get a box</Link>
+//     </li>
+// </ul>
+// </nav>
 
 export default Header
